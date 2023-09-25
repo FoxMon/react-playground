943c6217d5fd4102897e405547c0e3eec9a07836
foxmon <jjockrod@naver.com>
2023-09-25 11:24:42 +0900

    with suspense

---
+++ src/hoc/withSuspense.tsx
@@ -0,0 +1,27 @@
+import { Suspense, ComponentType, ReactNode } from "react";
+
+interface WithSuspenseProps {
+  WrappedComponent: ComponentType;
+  fallbackElement: ReactNode;
+}
+
+const LoadingComponent = () => {
+  return <div>Loading...</div>;
+};
+
+export const withSuspense = ({
+  WrappedComponent,
+  fallbackElement,
+}: WithSuspenseProps) => {
+  const ComponentWithSuspense = () => {
+    if (!fallbackElement) {
+      fallbackElement = LoadingComponent as unknown as ReactNode;
+    }
+
+    return (
+      <Suspense fallback={fallbackElement}>
+        <WrappedComponent />
+      </Suspense>
+    );
+  };
+};
