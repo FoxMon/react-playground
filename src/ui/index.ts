9794240806f305dc9b8c16a3fd7dafddb91e01c2
foxmon <jjockrod@naver.com>
2023-09-27 20:27:00 +0900

    ui

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,6 @@
 import { ButtonGroups } from "./ButtonGroups";
+import { ImageComp } from "./Image";
 import { Skeleton } from "./Skeleton";
 import { Spinner } from "./Spinner";
 
-export { ButtonGroups, Skeleton, Spinner };
+export { ButtonGroups, ImageComp, Skeleton, Spinner };
