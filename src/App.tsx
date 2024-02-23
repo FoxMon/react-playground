8002edc5cf8369b872994be9131cf12f9a1114ea
foxmon <jjockrod@naver.com>
2024-02-23 11:18:13 +0900

    rq

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
+import { Suspense } from "react";
+import {
+  QueryClientProvider,
+  QueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./assets/theme";
 import { GlobalStyle } from "./assets/global";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <div></div>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <div></div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <QueryExample />
+        </Suspense>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
+}
+
+function QueryExample() {
+  const { error, data } = useSuspenseQuery({
+    queryKey: ["fakestoreapi/products"],
+    queryFn: () =>
+      fetch("https://fakestoreapi.com/products").then((res) => res.json()),
+  });
+
+  console.log(error);
+  console.log(data);
+
+  return <div>Query</div>;
 }
 
 export default App;
